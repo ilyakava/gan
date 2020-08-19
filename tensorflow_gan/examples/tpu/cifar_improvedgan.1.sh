eb6f40f4f84277aa6ab134d6b2f7d0588f99ed9f
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-08-19 13:15:54 -0400

    WIP

---
+++ tensorflow_gan/examples/tpu/cifar_improvedgan.1.sh
@@ -0,0 +1,15 @@
+
+export EXPERIMENT_NAME=cifar_improved_4step_2eqlr
+export BATCH_SIZE=64
+export TRAIN_STEPS_PER_EVAL=10000
+export DATASET_ARGS='--image_size=32 --dataset_name=cifar10 --num_classes=10 --dataset_val_split_name=test'
+
+
+export ADDITIONAL='--critic_type=kplusone_fm \
+--generator_loss_fn=kplusone_featurematching_generator_loss \
+--kplusone_nll_discriminator_weight=1.0 \
+--tpu_gan_estimator_d_step=4 \
+--discriminator_lr=0.0002 \
+--generator_lr=0.0002'
+
+bash tpu/_base.sh

286f102545c849963305d932c6ce540a7eb27932
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-08-19 15:52:30 -0400

    WIP

--- tensorflow_gan/examples/tpu/cifar_improvedgan.sh
+++ tensorflow_gan/examples/tpu/cifar_improvedgan.sh
@@ -1,5 +1,5 @@
 
-export EXPERIMENT_NAME=cifar_improved_1step
+export EXPERIMENT_NAME=cifar_improved_4step
 export BATCH_SIZE=64
 export TRAIN_STEPS_PER_EVAL=10000
 export DATASET_ARGS='--image_size=32 --dataset_name=cifar10 --num_classes=10 --dataset_val_split_name=test'
@@ -7,6 +7,10 @@ export DATASET_ARGS='--image_size=32 --dataset_name=cifar10 --num_classes=10 --d
 
 export ADDITIONAL='--critic_type=kplusone_fm \
 --generator_loss_fn=kplusone_featurematching_generator_loss \
---kplusone_nll_discriminator_weight=1.0'
+--kplusone_nll_discriminator_weight=1.0 \
+--tpu_gan_estimator_d_step=4 \
+--extra_eval_metrics \
+--eval_batch_size=1024 \
+--num_eval_steps=9'
 
 bash tpu/_base.sh

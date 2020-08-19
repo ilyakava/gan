34b201783bad7150d7a8ca994b0abcb16b6b8795
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-08-19 15:01:27 -0400

    WIP

--- tensorflow_gan/examples/tpu/cifar_complementgan.sh
+++ tensorflow_gan/examples/tpu/cifar_complementgan.sh
@@ -11,6 +11,9 @@ export ADDITIONAL='--critic_type=kplusone_fm \
 --kplusone_nll_discriminator_weight=1.0 \
 --tpu_gan_estimator_d_step=4 \
 --discriminator_lr=0.0002 \
---generator_lr=0.0002'
+--generator_lr=0.0002 \
+--extra_eval_metrics \
+--eval_batch_size=1024 \
+--num_eval_steps=9'
 
 bash tpu/_base.sh

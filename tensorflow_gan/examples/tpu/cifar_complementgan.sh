e15a2a1921fed3eb4424ad8065de06005bf42b46
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-08-20 14:05:54 -0400

    WIP

--- tensorflow_gan/examples/tpu/cifar_complementgan.sh
+++ tensorflow_gan/examples/tpu/cifar_complementgan.sh
@@ -1,5 +1,5 @@
 
-export EXPERIMENT_NAME=cifar_complement_4step_2eqlr_conf0p5
+export EXPERIMENT_NAME=cifar_complement_4step_conf0p5
 export BATCH_SIZE=64
 export TRAIN_STEPS_PER_EVAL=10000
 export DATASET_ARGS='--image_size=32 --dataset_name=cifar10 --num_classes=10 --dataset_val_split_name=test'
@@ -10,8 +10,6 @@ export ADDITIONAL='--critic_type=kplusone_fm \
 --kplusonegan_confuse_generator_weight=0.5 \
 --kplusone_nll_discriminator_weight=1.0 \
 --tpu_gan_estimator_d_step=4 \
---discriminator_lr=0.0002 \
---generator_lr=0.0002 \
 --extra_eval_metrics \
 --eval_batch_size=1024 \
 --num_eval_steps=9'

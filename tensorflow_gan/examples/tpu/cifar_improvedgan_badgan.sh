f208f27d0235018ceccbe6dc1cf0805c4b63abe7
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-08-21 15:48:40 -0400

    WIP

--- tensorflow_gan/examples/tpu/cifar_improvedgan_badgan.sh
+++ tensorflow_gan/examples/tpu/cifar_improvedgan_badgan.sh
@@ -1,11 +1,11 @@
 
-export EXPERIMENT_NAME=cifar_improved_badgan
+export EXPERIMENT_NAME=cifar_improved_badgan1
 export BATCH_SIZE=128
 export TRAIN_STEPS_PER_EVAL=10000
 export DATASET_ARGS='--image_size=32 --dataset_name=cifar10 --num_classes=10 --dataset_val_split_name=test'
 
 
-export ADDITIONAL='--critic_type=kplusone_fm \
+export ADDITIONAL='--critic_type=kplusone_fm_badgan \
 --generator_loss_fn=kplusone_featurematching_generator_loss \
 --kplusone_nll_discriminator_weight=1.0 \
 --tpu_gan_estimator_d_step=1 \

4f3c3020f992e477affcb0958ac912f6df8b45cc
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-08-24 14:49:05 -0400

    WIP

--- tensorflow_gan/examples/tpu/cifar_complementgan.1.sh
+++ tensorflow_gan/examples/tpu/cifar_complementgan.1.sh
@@ -1,17 +1,15 @@
 
-export EXPERIMENT_NAME=cifar_complement_1step_conf0p1_lrelu_dropout
-export BATCH_SIZE=128
-export TRAIN_STEPS_PER_EVAL=25000
+export EXPERIMENT_NAME=cifar_complement_4step_conf0p5_lrelu_dropout
+export BATCH_SIZE=68
+export TRAIN_STEPS_PER_EVAL=20000
 export DATASET_ARGS='--image_size=32 --dataset_name=cifar10 --num_classes=10 --dataset_val_split_name=test'
 
 
 export ADDITIONAL='--critic_type=kplusone_fm_lrelu_dropout \
 --generator_loss_fn=kplusonegan_csc_generator_loss \
---kplusonegan_confuse_generator_weight=0.1 \
+--kplusonegan_confuse_generator_weight=0.5 \
 --kplusone_nll_discriminator_weight=1.0 \
---tpu_gan_estimator_d_step=1 \
---discriminator_lr=0.0006 \
---generator_lr=0.0003 \
+--tpu_gan_estimator_d_step=4 \
 --extra_eval_metrics \
 --eval_batch_size=1024 \
 --num_eval_steps=9 \
